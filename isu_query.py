5f575c7da31c8c95eb77750358d4d964fd46224c
Evgeny Cherkashin <eugeneai@irnok.net>
2022-05-10 02:24:43 +0800

    Added exporting into RDF

--- isu_query.py
+++ isu_query.py
@@ -1,4 +1,11 @@
 import requests as rq
+from rdflib import (Namespace, URIRef,
+                    Literal, BNode, FOAF,
+                    DC, DCTERMS, Graph,
+                    RDF, RDFS, XSD
+                    )
+
+
 
 ENDPOINT = 'http://py.isu.ru:8000/hs/jsonpost/courses_in_faculty/'
 USER = "3c9467d8-b710-11e6-943c-005056100702"
@@ -17,7 +24,8 @@ def query(user, faculty=IMIT, profile=None, year=None, term=None):
     `faculty` is name of a faculty/institute,
     `profile` is the identifier of a profile/specialty
               (not used now),
-    `year` is the year of study start,
+    `year` is the year of study start
+              (not used in requests),
     `term` is the semester mark (SEMAUTUMN, SEMSPRING)
     """
     if term is None:
@@ -36,6 +44,102 @@ def query(user, faculty=IMIT, profile=None, year=None, term=None):
         print('Return code:', resp.status_code)
         raise RuntimeError('request failed ({})'.format(resp.status_code))
 
+IDB = Namespace("http://irnok.net/ontologies/database/isu/studplan#")
+IDD = Namespace("http://irnok.net/ontologies/isu/studplan#")
+DBR = Namespace("https://dbpedia.org/page/")
+
+MAP = {
+    'ВидКонтроля': IDD['controlType'],
+    'Дисциплина': IDD['discipline'],
+    'Лабораторные': IDD['laboratoryWorksAmount'],
+    'Лекции': IDD['lectionAmount'],
+    'ПериодКонтроля': IDD['controlPeriod'],
+    'Практические': IDD['practiceAmount'],
+    'Профиль': IDD['profile'],
+    'ТипЗаписи': IDD['recordType'],
+    'УчебныйПлан': IDD['studyPlan'],
+    'УчебныйПланСпециальность': IDD['specialty'],
+    'УчебныйПланУровеньПодготовки': IDD['level'],
+    'УчебныйПланФормаОбучения': IDD['studyForm']
+}
+
+DT = {
+    int: {'datatype' : XSD.integer},
+    str: {'lang' : u'ru'}
+}
+
+TERM = {
+    0 : IDD['Even'],
+    1 : IDD['Odd'],
+}
+
+
+def tograph(query, graph=None, catalogs=None):
+    j = query
+    if (j['error_status']):
+        raise RuntimeError('wrong return status')
+    j = j['hs_json']
+    i = j['Input']
+    o = j['Output']
+    dj = o['Data'] # list of courses
+    if graph is None:
+        g = Graph()
+        g.bind('idb', IDB)
+        g.bind('idd', IDD)
+        g.bind('dbr', DBR)
+        g.add((TERM[0], RDFS.label, Literal('четный', lang=u'ru')))
+        g.add((TERM[1], RDFS.label, Literal('нечетный', lang=u'ru')))
+        g.add((TERM[0], RDFS.label, Literal('even', lang=u'en')))
+        g.add((TERM[1], RDFS.label, Literal('odd', lang=u'en')))
+    else:
+        g = graph
+    term = TERM[term]
+    faculty = IDB[i['facultet']]
+
+    if catalogs is None:
+        catalogs = {} # Various catalogs
+
+    def _cat(node):
+        t = type(node)
+        if t in [int, str]:
+            kwargs1 = DT[t]
+            return Literal(node, **kwargs1)
+        if node['type'] in ['CatalogRef', 'DocumentRef']:
+            cat = catalogs.setdefault(node['catalog'], {})
+            uid = node['uid']
+            cat[uid] = node['name']
+            return URIRef(uid)
+
+    g.add((faculty, RDF.type, DBR.Faculty))
+    for cour in dj:
+        dis = BNode()
+        g.add((faculty, IDD['hasDiscipline'], dis))
+        g.add((dis, IDD['term'], term))
+        for k, node in cour.items():
+            subj = _cat(node)
+            g.add((dis, MAP[k], subj))
+    return g
+
+
+def setupcatalogs(graph, catalogs):
+    g = graph
+    for kc, cat in catalogs.items():
+        for uid, name in cat.items():
+            uid = URIRef(uid)
+            g.add((uid, RDFS['label'], Literal(name, lang=u"ru")))
+            g.add((uid, RDF.type, IDD[kc]))
+    return g
+
 
 if __name__ == '__main__':
-    print(query(user=USER, faculty=IMIT, term=SEMSPRING))
+    # from pprint import pprint
+    q = query(user=USER, faculty=IMIT, term=SEMSPRING)
+    catalogs = {}
+    print('Processing spring')
+    g = tograph(q, catalogs=catalogs)
+    q = query(user=USER, faculty=IMIT, term=SEMAUTUMN)
+    print('Processing autumn')
+    g = tograph(q, graph=g, catalogs=catalogs)
+    print('Processing catalogs')
+    setupcatalogs(g, catalogs)
+    g.serialize('studplan.ttl', format='turtle')
