fbf6f6f46f99fb1b1d2baab0d2d96a6ad0fca55a
Evgeny Cherkashin <eugeneai@irnok.net>
2022-06-09 16:50:15 +0800

    Moving to recurse

--- distiller.py
+++ distiller.py
@@ -9,28 +9,31 @@ logging.basicConfig(format='%(levelname)s:%(module)s:%(message)s',
                     level=logging.DEBUG)
 
 
-
 def distill(uri, html, idgen=BNode):
     tree = etree.HTML(html)
     g = Graph()
     binds(g)
     nsm = g.namespaces()
-    NS = {ns:ur for ns, ur in nsm}
+    NS = {ns: ur for ns, ur in nsm}
     print(NS["wpdd"])
+
     def ext(definition):
         definition = definition.strip()
         a = definition.split(":", maxsplit=1)
+
+        if isinstance(definition, (URIRef, Literal, BNode)):
+            return definition
 
         if definition.startswith("http://") or \
              definition.startswith("https://") or \
              definition.startswith("file:/"):
             return URIRef(definition)
-        elif len(a)>1:
+        elif len(a) > 1:
             pr, rest = a
             ns = NS.get(pr, None)
             if ns is None:
                 logger.error("Unknown namespace '{}'".format(pr))
-                ns = pr+":"
+                ns = pr + ":"
             return URIRef(ns + rest)
         else:
             return g.absolutize(definition)
@@ -39,18 +42,21 @@ def distill(uri, html, idgen=BNode):
         g.add((ext(subj), ext(pred), ext(obj)))
         # print("<{} {} {}>".format(subj, pred, obj))
         # print("<{} {} {}>".format(ext(subj), ext(pred), ext(obj)))
+
     # <subj, pred, obj>
     subj = URIRef(uri)
     pred = []
     rev = []
     obj = None
     types = []
-    for element in tree.iter():
+    for element in tree.iterchildren():
+        if isinstance(element, etree._Comment):
+            continue
         kwargs = {}
         print(element, element.attrib)
         attrs = element.attrib
         if "lang" in attrs:
-            kwargs["lang"]=attrs["lang"]
+            kwargs["lang"] = attrs["lang"]
         if "property" in attrs:
             pred = attrs["property"].strip().split()
         if "rev" in attrs:
@@ -67,7 +73,7 @@ def distill(uri, html, idgen=BNode):
             obj = Literal(attrs["href"], **kwargs)
         elif element.text is not None:
             obj = Literal(element.text, **kwargs)
-        if obj is not None and ( pred or rev ):
+        if obj is not None and (pred or rev):
             for t in types:
                 gen(obj, RDF.type, t)
             for p in pred:
@@ -83,9 +89,7 @@ def distill(uri, html, idgen=BNode):
     return g
 
 
-
-
-TEST="""<div property="wpdd:itemList" typeof="wpdd:ItemList wpdd:QuestionList" class="item-list">
+TEST = """<div property="wpdd:itemList" typeof="wpdd:ItemList wpdd:QuestionList" class="item-list">
           <ol id="listeditor-Question">
             <li class="edit-itemlist" rev="schema:member" typeof="wpdd:ListItem wpdd:Question">
               <div property="rdfs:label" lang="ru" class="edit-list-label" contenteditable="true">Ответ</div>
@@ -104,8 +108,8 @@ TEST="""<div property="wpdd:itemList" typeof="wpdd:ItemList wpdd:QuestionList" c
           <!-- Кнопки -->
         </div>"""
 
-WPURI=WPDD["123434587195871958734"]
+WPURI = WPDD["123434587195871958734"]
 
-if __name__=="__main__":
+if __name__ == "__main__":
     g = distill(WPURI, TEST, idgen=lambda: genuuid(WPDB))
-    g.serialize(destination = "distil.ttl", encoding="utf8", format="turtle")
+    g.serialize(destination="distil.ttl", encoding="utf8", format="turtle")
